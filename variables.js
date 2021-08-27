38e2c963978c8fe0e7de1d0ab9310615e35e2090
PiperPimientos <feliperestrepomeneses@gmail.com>
2021-08-26 23:46:53 -0500

    agregamos los archivos funciones.js y variables.js para explicar los fundamentos de cada uno

---
+++ variables.js
@@ -0,0 +1,13 @@
+var nombre = "Oscar"
+
+//declarar
+var edad;
+// inicializar
+edad = 30;
+// mas compleja
+var elementos = ["Computadora", "Celular"];
+//tipo objeto
+var persona = {
+    nombre: "Diego",
+    edad: 30
+}
